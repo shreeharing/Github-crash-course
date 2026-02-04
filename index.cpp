d56bc664aaa9a5063c0c9922766cd9c2e66187e6
Shree Hari N G <shreeharing20@gmail.com>
2026-02-04 16:53:51 +0530

    Added subtraction function

--- index.cpp
+++ index.cpp
@@ -6,9 +6,14 @@ int addition(int a, int b){
     return a+b;
 }
 
+int subtraction(int a, int b){
+    return a-b;
+}
+
 int main(){
     int abc = 20;
     cout<<abc<<endl;
     cout<<addition(20,10);
+    cout<<subtraction(20,10);
     return 0;
 }
