5c23f95a63c2d93627b4d5c63052886851508ef4
Shree Hari N G <shreeharing20@gmail.com>
2026-02-08 21:52:41 +0530

    new code added for new branch

--- branch.cpp
+++ branch.cpp
@@ -2,8 +2,12 @@
 
 using namespace std;
 
+void func(){
+    cout<<"This line of code is added after crating the new branch"<<endl;
+}
+
 int main(){
-    
+
     cout<<"This file is on;y for Hari's Branch"<<endl;
 
     return 0;
