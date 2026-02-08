73e04aa936189e2cdc107e30b3323907e9935583
Shree Hari N G <shreeharing20@gmail.com>
2026-02-08 22:40:04 +0530

    commit-1

--- branch.cpp
+++ branch.cpp
@@ -6,6 +6,11 @@ void func(){
     cout<<"This line of code is added after crating the new branch"<<endl;
 }
 
+
+void func2(){
+    cout<<"This line of code is added after crating the new branch 2"<<endl;
+}
+
 int main(){
 
     cout<<"This file is on;y for Hari's Branch"<<endl;
