19da9ec8cd4fec5fda539fb0a73d4df3c0de7940
Shree Hari N G <shreeharing20@gmail.com>
2026-02-08 22:40:47 +0530

    commit-2

--- branch.cpp
+++ branch.cpp
@@ -9,6 +9,8 @@ void func(){
 
 void func2(){
     cout<<"This line of code is added after crating the new branch 2"<<endl;
+
+    cout<<"Lets just use this line for next commit"<<endl;
 }
 
 int main(){
